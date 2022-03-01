82ad7096eb38f9e11f932bfd2754b9f36c06a2ef
ThePhilosopher4097 <santdnyanrajmauli@gmail.com>
2022-03-01 17:37:08 +0530

    01-03-2022 NodeJS

--- NodeJS/Node Tutorials/Node_3.js
+++ NodeJS/Node Tutorials/Node_3.js
@@ -2,4 +2,10 @@ const http = require('http')
 
 const server = http.createServer()
 
+server.on('connection', (socket) => {
+    console.log("New Connection")
+})
+
 server.listen(3000)
+
+console.log("Server Listening at port 3000 ")
