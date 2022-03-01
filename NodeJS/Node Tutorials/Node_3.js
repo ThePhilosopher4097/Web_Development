9c6a5feea415ad4e0b0420ad25a913d2e514a958
ThePhilosopher4097 <santdnyanrajmauli@gmail.com>
2022-03-01 17:42:56 +0530

    01-03-2022 NodeJS

--- NodeJS/Node Tutorials/Node_3.js
+++ NodeJS/Node Tutorials/Node_3.js
@@ -1,6 +1,11 @@
 const http = require('http')
 
-const server = http.createServer()
+const server = http.createServer(function (request, response) {
+    if(request.url === '/') {
+        response.write('Welcome to BMC !')
+        response.end()
+    }
+})
 
 server.on('connection', (socket) => {
     console.log("New Connection")
